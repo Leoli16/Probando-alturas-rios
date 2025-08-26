5a4a0bef2bef6768269212fcb40007c2c2b4f71c
Leonel Abud <abudleito@gmail.com>
2025-08-26 13:55:09 -0300

    Listo

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
 
 import fs from "fs"
-import { text } from "stream/consumers";
+
 async function probar(){
     let series = await fetch("https://alerta.ina.gob.ar/pub/datos/series&&format=json");
     series = await series.json();
     series = series.data;
-
+    console.log(series);
     let disponibles = []
     for(let i in series){
+        console.log(i)
         if (series[i].var_nombre === "Altura hidrométrica"){
-            let result = await fetch(`https://alerta.ina.gob.ar/pub/datos/datosDia&date=2025-08-21&seriesId=${series[i].seriesid}&desc=1&siteCode=${series[i].sitecode}&varId=2&procId=1&format=json`);
+            let result = await fetch(`https://alerta.ina.gob.ar/pub/datos/datosDia&date=2025-08-26&seriesId=${series[i].seriesid}&desc=1&siteCode=${series[i].sitecode}&varId=2&procId=1&format=json`);
             result = await result.text();
             result = result.slice(0,28);
             result+="}"
-            result = JSON.parse(result);
-            if (Object.hasOwn(result, 'title') == false){
-                console.log(`se cumple - ${i}/${series.length}`);
-                disponibles.push(series[i]);
-            }else{
-                console.log(`No se cumple - ${i}/${series.length}`)
+            try{
+                result = JSON.parse(result);
+                if (Object.hasOwn(result, 'title') == false){
+                    console.log(`se cumple - ${i}/${series.length}`);
+                    disponibles.push(series[i]);
+                }else{
+                    console.log(`No se cumple - ${i}/${series.length}`)
+                }
+            }catch(error){
+                if (Object.hasOwn(result, 'title') == false){
+                    console.log(`se cumple - ${i}/${series.length}`);
+                    disponibles.push(series[i]);
+                }else{
+                    console.log(`No se cumple - ${i}/${series.length}`)
+                }
             }
+
         }
         
     }
