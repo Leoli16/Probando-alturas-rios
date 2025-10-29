d1425d7fdcae9a3e9a1d3030f2592b56eebfbcd2
Leonel Abud <abudleito@gmail.com>
2025-10-29 18:53:03 -0300

    Coordenadas asignadas

--- asignarCoordenadas.js
+++ asignarCoordenadas.js
@@ -1,7 +1,10 @@
     import fs from "fs"
+import { json } from "stream/consumers";
 
 let localidades = fs.readFileSync("localidades.json");
+localidades = JSON.parse(localidades)
 let inundaciones = fs.readFileSync("inundaciones.json");
+inundaciones = JSON.parse(inundaciones)
 
 for (let i in inundaciones){
     let codeIn = inundaciones[i]["Code Departamento"]
@@ -9,12 +12,28 @@ for (let i in inundaciones){
         let codeLoc = localidades[w]["Code Departamento"]
         if (codeIn.length === 4){
             if ("0"+codeIn === codeLoc){
-                "seguir"
+                inundaciones[i].lat = localidades[w].lat
+                inundaciones[i].lon = localidades[w].lon
+            }
+            else if (codeIn === "6378"){
+                inundaciones[i].lat = "-34.4627196"
+                inundaciones[i].lon = "-58.7739423"
             }
         }else{
             if (codeIn === codeLoc){
-                "seguir con esto"
+                inundaciones[i].lat = localidades[w].lat
+                inundaciones[i].lon = localidades[w].lon
             }
         }
     }
+}
+
+let lista = inundaciones
+const contenidoJSON = JSON.stringify(lista, null, 2);
+
+try {
+    fs.writeFileSync('pruebaFetchs.json', contenidoJSON);
+    console.log('✅ ¡Archivo "pruebaFetchs.json" guardado con éxito!');
+  } catch (error) {
+    console.error('❌ Error al guardar el archivo:', error);
 }
