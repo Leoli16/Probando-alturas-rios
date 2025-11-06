ea96d02193c7db200e37c7cc35200f6fae5e6591
Leonel Abud <abudleito@gmail.com>
2025-11-06 12:12:25 -0300

    se me queda sin bateria la compuu

--- promediarInundaciones.js
+++ promediarInundaciones.js
@@ -5,18 +5,24 @@ let dataset = {}
 let fecha = "uysdkjf"
 
 for (let i in datosDeRios){
-    let valoresIguales = []
-    dataset[i] = []
+    let valoresIguales = [];
+    dataset[i] = [];
+    let primeraVez = true;
     for (let x of datosDeRios[i]){
         if (x.timestart.slice(0,10) === fecha){
             valoresIguales.push(x.valor);
         }
         else{
-            let sumador = 0;
-            for (let s of valoresIguales){
-                sumador+=s;
+            if (!primeraVez){
+                let sumador = 0;
+                for (let s of valoresIguales){
+                    sumador+=s;
+                }
+                dataset[i].push({valor: sumador/valoresIguales.length, timestart: fecha})
+            }else{
+                primeraVez= false
             }
-            dataset[i].push({valor: sumador/valoresIguales.length, timestart: fecha})
+
         }
         fecha = x.timestart.slice(0,10);
     }
