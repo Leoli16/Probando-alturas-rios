c909188cc5a87942560041851517112e1e9c30bd
NicoBatti <nicolasbattias@gmail.com>
2025-11-12 10:59:30 -0300

    coordenadas agregadas a estaciones de rios

--- AgregarCoordenadas.js
+++ AgregarCoordenadas.js
@@ -3,25 +3,26 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
 import fs from "fs"
 import fetch from 'node-fetch';
 
-let lista = fs.readFileSync("inundaciones.json");
+let lista = fs.readFileSync("alturasHidrometricas.json");
 lista = JSON.parse(lista);
 
-
-
+let listaDeOriginales = await fetch("https://alerta.ina.gob.ar/pub/datos/estaciones&&format=json");
+listaDeOriginales = await listaDeOriginales.json();
+listaDeOriginales = listaDeOriginales.data;
 
 for (let i in lista){
-    let provincia = lista[i]["Provincia"];
-    let localidad = lista[i]["Departamento"];
-    console.log(`${parseInt(i)+1}/${lista.length}`);
-    let data = await fetch(`https://nominatim.openstreetmap.org/search?q=${localidad},${provincia},argentina&format=json`);
-    data = await data.json();
-    console.log(data)
+    for (let w of listaDeOriginales){
+        if (w.sitecode === lista[i].sitecode){
+            lista[i].lat = w.lat;
+            lista[i].lon = w.lon;
+        }
+    }
 }
 
 const contenidoJSON = JSON.stringify(lista, null, 2);
 
 try {
-    fs.writeFileSync('inundaciones.json', contenidoJSON);
+    fs.writeFileSync('alturasHidrometricas.json', contenidoJSON);
     console.log('✅ ¡Archivo "lluvias.json" guardado con éxito!');
   } catch (error) {
     console.error('❌ Error al guardar el archivo:', error);
