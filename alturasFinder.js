446c13e0db806d4d3472768e51070e80fcc17c8b
Leonel Abud <abudleito@gmail.com>
2025-11-12 10:44:34 -0300

    agregar todos los rios

--- alturasFinder.js
+++ alturasFinder.js
@@ -1,5 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
 
+import fetch from "node-fetch";
 import fs from "fs"
 
 async function probar(){
@@ -10,35 +11,15 @@ async function probar(){
     let disponibles = []
     for(let i in series){
         console.log(i)
-        if (series[i].var_nombre === "Altura hidrométrica"){
-            let result = await fetch(`https://alerta.ina.gob.ar/pub/datos/datosDia&date=2025-08-26&seriesId=${series[i].seriesid}&desc=1&siteCode=${series[i].sitecode}&varId=2&procId=1&format=json`);
-            result = await result.text();
-            result = result.slice(0,28);
-            result+="}"
-            try{
-                result = JSON.parse(result);
-                if (Object.hasOwn(result, 'title') == false){
-                    console.log(`se cumple - ${i}/${series.length}`);
-                    disponibles.push(series[i]);
-                }else{
-                    console.log(`No se cumple - ${i}/${series.length}`)
-                }
-            }catch(error){
-                if (Object.hasOwn(result, 'title') == false){
-                    console.log(`se cumple - ${i}/${series.length}`);
-                    disponibles.push(series[i]);
-                }else{
-                    console.log(`No se cumple - ${i}/${series.length}`)
-                }
-            }
-
+        if (series[i].varid === 2 && series[i].procid === 1){
+            disponibles.push(series[i])
         }
         
     }
     const contenidoJSON = JSON.stringify(disponibles, null, 2);
 
     try {
-        fs.writeFileSync('disponibles.json', contenidoJSON);
+        fs.writeFileSync('alturasHidrometricas.json', contenidoJSON);
         console.log('✅ ¡Archivo "usuarios.json" guardado con éxito!');
       } catch (error) {
         console.error('❌ Error al guardar el archivo:', error);
